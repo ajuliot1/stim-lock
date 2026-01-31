e4839307781895a41bca28024fa63cffdc493e69
Andrew Juliot <60113876+ajuliot1@users.noreply.github.com>
2026-01-30 21:24:03 -0600

    Revise content and update video link in page.jsx
    Updated content to specify neurostimulator procedures and changed video link for demonstration.

--- app/page.jsx
+++ app/page.jsx
@@ -16,18 +16,19 @@ The card content below is fetched by the client-side from \`/quotes/random\` (se
 `;
 
 const ctx = getNetlifyContext();
-
+console.log("THIS IS RUNNING")
 export default function Page() {
     return (
         <div>
             <section className='full-width-image'>
+
 
                 {/* Could use this for "Patient use only" kind of disclaimers: */}
                 {/* <ContextAlert className="mb-6" /> */}
 
                 <h1 className="mb-4" style={{ fontWeight: '400', fontFamily: 'Inter' }}><b>Ensure Post‑Op Stability</b> with Confidence</h1>
                 <p className="mb-6" style={{ fontFamily: 'Inter', fontWeight: '300', paddingLeft: '10vw', paddingRight: '10vw'}}>
-                    Stim-Lock devices simplify and protect [name of kind of surgery] procedures, providing peace of mind for clinicians and patients alike.
+                    Stim-Lock devices simplify and protect percutaneously implanted neurostimulator  procedures, providing peace of mind for clinicians and patients alike.
                 </p>
 
             </section>
@@ -36,11 +37,11 @@ export default function Page() {
                 <div className="overview">
                     <h1 className="mb-4" style={{ fontWeight: '700', fontFamily: 'Inter', color: '#1e1e1ed9'}}>Lock the Lead. Remove the Risk.</h1>
                     <p className="mb-6" style={{ fontFamily: 'Inter', fontWeight: '300', paddingLeft: '10vw', paddingRight: '10vw', maxWidth: '1000px' }}>
-                        [This is a short but clear description of how to use the product, allowing those who can't watch a video at the time of scanning this site to see the gist.]
+                        Stim‑Lock is a dedicated fixation device engineered to secure percutaneously implanted neurostimulator leads during trial procedures. By maintaining consistent lead stability throughout the trial period, Stim‑Lock supports surgical efficiency and helps ensure a more reliable assessment of therapeutic effectiveness.
                     </p>
 
                     <div className='button-container'>
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="btn btn-lg sm:min-w-64" style={{ fontFamily: 'Inter', fontSize: '1rem', marginTop: '20px', width: '100%', height: '52px'}}>
+                        <a target="_blank" rel="noopener noreferrer" href="https://vimeo.com/1160395772?share=copy&fl=sv&fe=ci" className="btn btn-lg sm:min-w-64" style={{ fontFamily: 'Inter', fontSize: '1rem', marginTop: '20px', width: '100%', height: '52px'}}>
                             See How it Works
                         </a>
                     </div>
@@ -48,6 +49,13 @@ export default function Page() {
 
                 <div className="flex justify-center">
                     <FeedbackForm />
+                </div>
+
+                <div className="overview" style={{paddingTop: '0', marginTop: '10px'}}>
+                    <h1 className="mb-4" style={{ fontSize: '16px', fontWeight: '700', fontFamily: 'Inter', color: '#1e1e1ed9' }}>DEMONSTRATION DEVICE – NOT FOR PATIENT USE</h1>
+                    <p className="mb-6" style={{ fontSize: '13px', fontFamily: 'Inter', fontWeight: '300', paddingLeft: '10vw', paddingRight: '10vw', maxWidth: '1000px' }}>
+                        This device sample is provided solely for purposes of demonstration and evaluation by healthcare professionals. It is not labeled for sale and any sale or resale is strictly prohibited. This sample is non-sterile and neither intended nor suitable for clinical use, whether in patient care, diagnosis, treatment, or any therapeutic application. Any use of this device sample outside of its intended demonstration purpose is strictly prohibited and the manufacturer assumes no responsibility for any misuse of this device. The manufacturer disclaims all warranties, express or implied, associated with this sample, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose, and expressly states that this sample has not been registered with the FDA, and has not been prepared, validated, or labeled for patient use. This device is protected by U.S. patent laws, and the manufacturer specifically reserves all associated intellectual property rights. Unauthorized use, reproduction, or replication is prohibited.
+                    </p>
                 </div>
 
                 {/* Could use this for customer reviews: */}
